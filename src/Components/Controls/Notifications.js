26fb9552f758234f01080a55a9319b48a2edf21b
moizsabun <moizzoaib786@gmail.com>
2021-03-08 23:17:30 +0500

    Added notification margin

--- src/Components/Controls/Notifications.js
+++ src/Components/Controls/Notifications.js
@@ -1,26 +1,39 @@
-import { Snackbar } from '@material-ui/core'
+import { makeStyles, Snackbar } from '@material-ui/core'
 import { Alert } from '@material-ui/lab'
 import React from 'react'
 
-export default function Notifications(props) {
-    const {notify,setNotify} = props
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-          return;
-        }
-    
-        setNotify ({
-            ...notify,
-            isOpen:false});
-      };
-    
-    return (
-     <Snackbar open={notify.isOpen} autoHideDuration= {2500} onClose={handleClose}>
-       <Alert severity={notify.type}>
-       {
-           notify.message
-       }</Alert>
-     </Snackbar>
+const useStyles = makeStyles(theme => (
+  {
+    root: {
+      top: theme.spacing(9)
+    }
+  }))
 
-    )
+export default function Notifications(props) {
+  const classes = useStyles();
+  const { notify, setNotify } = props
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setNotify({
+      ...notify,
+      isOpen: false
+    });
+  };
+
+  return (
+    <Snackbar open={notify.isOpen} 
+    autoHideDuration={2500} 
+    onClose={handleClose} 
+    className={classes.root}
+    anchorOrigin = { {vertical : 'top' , horizontal : 'right'}}>
+      <Alert severity={notify.type} onClose={handleClose}>
+        {
+          notify.message
+        }</Alert>
+    </Snackbar>
+
+  )
 }
